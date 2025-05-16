b22625231f2e7b434b43f4a52e1e40d7347a02ea
von-solano <a1890523@adelaide.edu.au>
2025-05-16 23:11:51 +0930

    mini shell: main structure for mini shell

--- main.c
+++ main.c
@@ -1,33 +1,43 @@
+// entry point to mini shell
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
+
+#include "launch_command.h"
+#include "parse_line.h"
+#include "read_line.h"
+#include "run_command.h"
 
 // loop structure for mini shell
-void mini_shell_loop(void){
-    char* line;
-    char **args;
-    int status;
+void ms_loop(void) {
+  char* line;
+  char** args;
+  int status;
 
-    // do while-loop (check status variable after)
-    do {
-        printf("mini_shell> ");
-        line = read_line(); // read in line from user
-        args = parse_line(); // split line into command and arguments
-        status = run_command(args); // execute command
+  // do while-loop (check status variable after)
+  do {
+    printf("mini_shell> ");
+    line = read_line();          // read in line from user
+    args = parse_line(line);     // split line into command and arguments
+    status = run_command(args);  // execute command
 
-        // free variables
-        free(line);
-        free (args);
-    } while(status); // loop until status is 0
+    // free variables
+    free(line);
+    if (args) {
+      free(args);
+    }
+  } while (status);  // loop until status is 0
 }
 
 // main function
-int main(int argc, char**argv){
+int main(int argc, char** argv) {
 
-    // run mini shell loop
-    mini_shell_loop();
-    
-    return 0;
+  printf("Welcome to Von's Mini Shell! Type 'help' to get started!\n");  
+  // run mini shell loop
+  ms_loop();
+
+  return 0;
 }
