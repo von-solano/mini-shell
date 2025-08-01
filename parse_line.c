5b9279377b4d522dd28d0475caaa3778ea90dc4c
von-solano <a1890523@adelaide.edu.au>
2025-08-01 15:06:48 +0930

    formatting: error messages and indentation

--- parse_line.c
+++ parse_line.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "read_line.h"
 
-#define BUFFER_SIZE 64          // set initial buffer size    
-#define DELIMITERS " \t\r\n\a"   // delimiters to split arguments from input line
+#define BUFFER_SIZE 64          // set initial buffer size
+#define DELIMITERS " \t\r\n\a"  // delimiters to split arguments from input line
 
-// take line and split into tokens
-char **parse_line(char* line){
-    int bufsize = BUFFER_SIZE; // starting buffer size of tokens array
-    int index = 0; // current position in tokens array
-    char** tokens = malloc(bufsize * sizeof(char*)); // array of tokens
+// parse line from stdin
+char** parse_line(char* line) {
+  int bufsize = BUFFER_SIZE;                        // starting buffer size of tokens array
+  int index = 0;                                    // current position in tokens array
+  char** tokens = malloc(bufsize * sizeof(char*));  // array of tokens
 
-    // check if memory allocation failed
-    if(!tokens){
+  // check if memory allocation failed
+  if (!tokens) {
+    fprintf(stderr, "ms: allocation error\n");
+    exit(EXIT_FAILURE);  // exit shell successfully
+  }
+
+  char* token = strtok(line, DELIMITERS);  // get first token
+
+  // loop through line and extract tokens
+  while (token != NULL) {
+    tokens[index] = strdup(token);  // store copy of current token in token array
+    index++;                        // increment position in token array
+
+    // if token array is full
+    if (index >= bufsize) {
+      bufsize += BUFFER_SIZE;  // double size of buffer
+      tokens = realloc(tokens, bufsize * sizeof(char*)); 
+
+      // check if reallocation failed
+      if (!tokens) {
         fprintf(stderr, "ms: allocation error\n");
-        exit(EXIT_FAILURE); // exit shell successfully
+        exit(EXIT_FAILURE);  // exit shell successfully
+      }
     }
+    token = strtok(NULL, DELIMITERS);  // get next token
+  }
 
-    char* token = strtok(line, DELIMITERS); // get first token
-
-    // loop through line and extract tokens
-    while(token != NULL){
-        tokens[index] = strdup(token); // store copy of current token in token array (so can be freed later)
-        index++; // increment position in token array
-
-        // if token array is full
-        if(index >= bufsize){
-            bufsize += BUFFER_SIZE;  // double size of buffer
-            tokens = realloc(tokens, bufsize * sizeof(char*)); // reallocate memory to increase space
-            
-            // check if reallocation failed
-            if(!tokens){
-            fprintf(stderr, "ms: allocation error\n");
-            exit(EXIT_FAILURE); // exit shell successfully
-            }
-        }
-
-        token = strtok(NULL, DELIMITERS); // get next token
-    }
-
-    tokens[index] = NULL; // terminate tokens array
-    return tokens; // return array of tokens
+  tokens[index] = NULL;  // terminate tokens array
+  return tokens;         // return array of tokens
 }
--- read_line.c
+++ read_line.c
@@ -1,22 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "read_line.h"
 
-// reading in line from user
-char *read_line(void){
-    char* line = NULL;       // store line from user
-    size_t buffer_size = 0; // variable to hold size of line buffer
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-    // read line from stdin
-    if(getline(&line, &buffer_size, stdin) == -1){
-        // if reading failed beacuse reached the end-of-file
-        if(feof(stdin)){
-            exit(EXIT_SUCCESS); // exit shell successfully
-        } else {
-            // if reading failed because of some other error
-            perror("readline");
-            exit(EXIT_FAILURE); // exit shell with error code
-        }
+// read line from stdin
+char* read_line(void) {
+  char* line = NULL;       // store line from user
+  size_t buffer_size = 0;  // variable to hold size of line buffer
+
+  // read line from stdin
+  if (getline(&line, &buffer_size, stdin) == -1) {
+    // if reading failed because reached end-of-file
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS);  // exit shell successfully
+    } else {
+      // if reading failed because of some other error
+      perror("readline");
+      exit(EXIT_FAILURE);  // exit shell with error code
     }
-    return line; // return input line
+  }
+  return line;  // return input line
 }
